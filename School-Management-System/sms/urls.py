73493747b024b7fa7626416f9c5b83a21aa2681b
Samuelcheche <Samuelonyango932@gmail.com>
2026-03-03 10:07:04 +0300

    new commit

--- School-Management-System/sms/urls.py
+++ School-Management-System/sms/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('accounts/', include('accounts.urls')),
     path('students/', include('students.urls')),
     path('teachers/', include('teachers.urls')),
